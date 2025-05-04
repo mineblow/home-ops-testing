92ba632bfa7d913c56177bfa4075002d3d2a405a
mineblow <ethan.roberts@mineblow.com>
2025-05-04 10:12:51 -0500

    da

--- terraform/environments/homelab/bootstrap-runner/provider.tf
+++ terraform/environments/homelab/bootstrap-runner/provider.tf
@@ -19,6 +19,11 @@ provider "proxmox" {
   endpoint  = var.proxmox_api_url
   api_token = var.proxmox_api_token
   insecure  = true
+
+  ssh {
+    username    = var.proxmox_ssh_user
+    private_key = file(var.proxmox_ssh_private_key)
+  }
 }
 
 provider "vault" {}
